6072ba6fb6f0ec94b90edf89fac8f95d06276283
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-07-18 14:59:22 +0200

    page de chargement fais sans faire exprès

--- src/component/API_KEY.jsx
+++ src/component/API_KEY.jsx
@@ -1,5 +1,5 @@
 const API_KEY =
-  "http://api.openweathermap.org/data/2.5/weather?q={input},uk&APPID=c7572f88749eaaba5bb2e1ae09edd35d";
+  "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c7572f88749eaaba5bb2e1ae09edd35d";
 
   // "http://api.openweathermap.org/data/2.5/weather?q={input},uk&APPID=c7572f88749eaaba5bb2e1ae09edd35d"
 
--- src/component/home.jsx
+++ src/component/home.jsx
@@ -2,28 +2,36 @@ import React, { useEffect, useState } from "react";
 import API_KEY from "./API_KEY";
 
 const Home = () => {
-
-  const [jsonData, setJsonData] = useState(null)
+  const [jsonData, setJsonData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await fetch(API_KEY)
-        const json = await data.json()
-        setJsonData(json)
-        console.log(json)
+        const data = await fetch(API_KEY);
+        const json = await data.json();
+        setJsonData(json);
+        console.log(json);
+      } catch (error) {
+        console.error("Une erreur s'est produite lors de la récupération des données", error);
       }
-    } catch (error) {
-      console.error("Une erreur s'est produite lors de la récuperation des données", error)
-    }
-  })  
+    };
 
-  fetchData()
+    fetchData();
+  }, []);
 
-}, []);
-
-
-
-  
+  return (
+    <div>
+      {jsonData ? (
+        <ul>
+          {jsonData.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Chargement en cours...</p>
+      )}
+    </div>
+  );
+};
 
 export default Home;
