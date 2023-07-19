bdfb9155879f28cbc02094307892c1061b5f6a13
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-07-19 14:47:53 +0200

    code encore casser

--- src/component/API_KEY.jsx
+++ src/component/API_KEY.jsx
@@ -1,6 +1,6 @@
-const API_KEY =
-  "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c7572f88749eaaba5bb2e1ae09edd35d";
+const getAPIKey = (inputCountryName) => {
+  const API_KEY = `http://api.openweathermap.org/data/2.5/weather?q=${inputCountryName},&APPID=c7572f88749eaaba5bb2e1ae09edd35d`;
+  return API_KEY;
+};
 
-  // "http://api.openweathermap.org/data/2.5/weather?q={input},uk&APPID=c7572f88749eaaba5bb2e1ae09edd35d"
-
-export default API_KEY;
+export default getAPIKey;
--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,24 +1,38 @@
 import React, { useEffect, useState } from "react";
-import API_KEY from "./API_KEY";
+import getAPIKey from "./API_KEY";
 
 const Home = () => {
   const [jsonData, setJsonData] = useState(null);
+  const [inputCountryName, setInputCountryName] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        const API_KEY = getAPIKey(inputCountryName);
         const data = await fetch(API_KEY);
         const json = await data.json();
+
+        // Note : la propriété 'list' n'existe pas dans l'objet JSON retourné par l'API
+        // Vous pouvez ajuster cette partie en fonction de la structure réelle de l'objet JSON retourné.
+
         const citiesArray = json.list; 
+
         setJsonData(citiesArray);
         console.log(citiesArray);
+        const imageLink = response.data.urls.regular
+        setBackgroundImage(imageLink)
       } catch (error) {
         console.error("Une erreur s'est produite lors de la récupération des données", error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [inputCountryName]);
+
+  const handleSearch = () => {
+    // fetch les données quand le button est cliqué
+    fetchData();
+  };
 
   return (
     <div>
@@ -29,7 +43,21 @@ const Home = () => {
           ))}
         </ul>
       ) : (
-        <p>Chargement en cours...</p>
+        <div>
+          <input
+            type="text"
+            value={inputCountryName}
+            onChange={(e) => setInputCountryName(e.target.value)}
+            placeholder="Country Name"
+          />
+          <button onClick={handleSearch}>Search</button>
+        <div
+        className="TropSimple"
+        style={{backgroundImage: `url(${backgroundImage})`}}
+        >
+
+        </div>
+        </div>
       )}
     </div>
   );
